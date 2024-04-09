7bc750a075e6db81755bcefb5b8d20e232d689f7
Paweł Dudek <95589776+PawelD16@users.noreply.github.com>
2024-04-09 18:24:47 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Określa dostawcę/ów dla Terraforma
 terraform {
   required_providers {
     aws = {
@@ -7,11 +8,13 @@ terraform {
   }
 }
 
+# Definiuje region, gdzie będą znadować się zasoby
 provider "aws" {
   region = "us-east-1"
 }
 
-
+# Tworzy Virtual Private Cloud, które pozwala na DNS i nazwy hostów.
+# Włącza wsparcie dla DNS i nazw hostów DNS wewnątrz VPC
 resource "aws_vpc" "app_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -21,6 +24,7 @@ resource "aws_vpc" "app_vpc" {
   }
 }
 
+# Brama umożliwiające łączenie się VPC z Internetem
 resource "aws_internet_gateway" "tic_tac_toe_igw" {
   vpc_id = aws_vpc.app_vpc.id
   tags = {
@@ -28,6 +32,7 @@ resource "aws_internet_gateway" "tic_tac_toe_igw" {
   }
 }
 
+# Tworzy podsieć w VPC z automatycznym przydzielaniem IP publicznych oraz dla EC2
 resource "aws_subnet" "tic_tac_toe_subnet" {
   vpc_id                  = aws_vpc.app_vpc.id
   cidr_block              = "10.0.1.0/24"
@@ -37,6 +42,7 @@ resource "aws_subnet" "tic_tac_toe_subnet" {
   }
 }
 
+# Tworzy tabelę routingu dla VPC, dodając trasę domyślną przez bramę 
 resource "aws_route_table" "tic_tac_toe_rt" { 
   vpc_id = aws_vpc.app_vpc.id
 
@@ -50,16 +56,20 @@ resource "aws_route_table" "tic_tac_toe_rt" {
   }
 }
 
+# Wiąże tabelę routingu z podsiecią, umożliwiając jej dostęp do internetu.
 resource "aws_route_table_association" "tic_tac_toe_rta" {
   subnet_id      = aws_subnet.tic_tac_toe_subnet.id
   route_table_id = aws_route_table.tic_tac_toe_rt.id
 }
 
-
+# Tworzy grupę bezpieczeństwa z regułami dla ruchu przychodzącego (ingress) i wychodzącego (egress), umożliwiając jedynie ruch na niezbędnych portach.
+# Zapewnia to bezpieczeństwo przed atakami na wolne porty.
 resource "aws_security_group" "tic_tac_toe_sg" {
   name        = "tic_tac_toe_sg"
   vpc_id      = aws_vpc.app_vpc.id
   description = "Security group for accessing application and ec2 via SSH"
+
+# HTTP
   ingress {
     from_port   = 80
     to_port     = 80
@@ -67,6 +77,7 @@ resource "aws_security_group" "tic_tac_toe_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+# HTTPS
   ingress {
     from_port   = 443
     to_port     = 443
@@ -74,13 +85,15 @@ resource "aws_security_group" "tic_tac_toe_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+# backend
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp" 
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
+# frontend (możnaby w sumie dawać front na port 80/443, ale zrobiłem inaczej)
   ingress {
     from_port   = 3000
     to_port     = 3000
@@ -88,6 +101,7 @@ resource "aws_security_group" "tic_tac_toe_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+# ssh
   ingress {
     from_port   = 22
     to_port     = 22
@@ -95,6 +109,7 @@ resource "aws_security_group" "tic_tac_toe_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Reguła dla całego ruchu wychodzącego, która go nie ogranicza
   egress {
     from_port   = 0
     to_port     = 0
@@ -107,6 +122,8 @@ resource "aws_security_group" "tic_tac_toe_sg" {
   }
 }
 
+# Tworzy instancję EC2 z określonym obrazem AMI, typem instancji i grupą bezpieczeństwa.
+# AMI zostało znalezione dzięki stronie https://api.netcubed.de/latest/ami/lookup?platform=amzn2&fbclid=IwAR0BKcf3D28DiuvttUfKMZE0WtwgWRv0kW9_XCCJwHBntSRfG1LbcFyYK10_aem_AWHs8WBIRtX7NEjqT6j_cNuemLt6UmXLj0LJI1Zhp8B1CU_acHz7FYUNf_YAMdEoUpdU0QfAvXFSdC-_Ml61ic2E
 resource "aws_instance" "tic_tac_toe_ec2" {
   ami                      = "ami-0cf43e890af9e3351"
   instance_type            = "t2.micro"
@@ -119,7 +136,7 @@ resource "aws_instance" "tic_tac_toe_ec2" {
   }
 }
 
-
+# Definicja i powiązanie Elastic IP, które daje nam publiczny adres IP
 resource "aws_eip" "app_eip" {
   domain     = "vpc"
   depends_on = [aws_vpc.app_vpc]
